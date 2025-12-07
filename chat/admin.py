c0f31932b86f7b67604304bfa8f0962411c46c2c
Lynette Ochieng <124053187+Cadeau3@users.noreply.github.com>
2025-12-07 21:26:15 +0300

    Create admin.py

---
+++ chat/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Room, Message
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("room","user","timestamp")
+    list_filter = ("room","timestamp")
